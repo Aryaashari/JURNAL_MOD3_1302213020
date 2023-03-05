eb8df010737112ac350d7f8c363aa66cc72ea09a
Arya Ashari <aryaashari100@gmail.com>
2023-03-06 01:07:52 +0700

    create kalkulator multiple values

--- modul2_1302213020/Form1.Designer.cs
+++ modul2_1302213020/Form1.Designer.cs
@@ -28,13 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.hasil = new System.Windows.Forms.Label();
+            this.tambah = new System.Windows.Forms.Button();
+            this.sama_dengan = new System.Windows.Forms.Button();
+            this.nol = new System.Windows.Forms.Button();
+            this.tiga = new System.Windows.Forms.Button();
+            this.dua = new System.Windows.Forms.Button();
+            this.satu = new System.Windows.Forms.Button();
+            this.enam = new System.Windows.Forms.Button();
+            this.lima = new System.Windows.Forms.Button();
+            this.empat = new System.Windows.Forms.Button();
+            this.sembilan = new System.Windows.Forms.Button();
+            this.delapan = new System.Windows.Forms.Button();
+            this.tujuh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hasil
+            // 
+            this.hasil.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hasil.AutoSize = true;
+            this.hasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hasil.Location = new System.Drawing.Point(37, 38);
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(21, 24);
+            this.hasil.TabIndex = 0;
+            this.hasil.Text = "0";
+            this.hasil.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.hasil.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.Location = new System.Drawing.Point(41, 79);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(30, 23);
+            this.tambah.TabIndex = 1;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // sama_dengan
+            // 
+            this.sama_dengan.Location = new System.Drawing.Point(77, 79);
+            this.sama_dengan.Name = "sama_dengan";
+            this.sama_dengan.Size = new System.Drawing.Size(30, 23);
+            this.sama_dengan.TabIndex = 2;
+            this.sama_dengan.Text = " =";
+            this.sama_dengan.UseVisualStyleBackColor = true;
+            this.sama_dengan.Click += new System.EventHandler(this.sama_dengan_Click);
+            // 
+            // nol
+            // 
+            this.nol.Location = new System.Drawing.Point(113, 79);
+            this.nol.Name = "nol";
+            this.nol.Size = new System.Drawing.Size(30, 23);
+            this.nol.TabIndex = 3;
+            this.nol.Text = " 0";
+            this.nol.UseVisualStyleBackColor = true;
+            this.nol.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tiga
+            // 
+            this.tiga.Location = new System.Drawing.Point(113, 108);
+            this.tiga.Name = "tiga";
+            this.tiga.Size = new System.Drawing.Size(30, 23);
+            this.tiga.TabIndex = 6;
+            this.tiga.Text = " 3";
+            this.tiga.UseVisualStyleBackColor = true;
+            this.tiga.Click += new System.EventHandler(this.tiga_Click);
+            // 
+            // dua
+            // 
+            this.dua.Location = new System.Drawing.Point(77, 108);
+            this.dua.Name = "dua";
+            this.dua.Size = new System.Drawing.Size(30, 23);
+            this.dua.TabIndex = 5;
+            this.dua.Text = " 2";
+            this.dua.UseVisualStyleBackColor = true;
+            this.dua.Click += new System.EventHandler(this.dua_Click);
+            // 
+            // satu
+            // 
+            this.satu.Location = new System.Drawing.Point(41, 108);
+            this.satu.Name = "satu";
+            this.satu.Size = new System.Drawing.Size(30, 23);
+            this.satu.TabIndex = 4;
+            this.satu.Text = "1";
+            this.satu.UseVisualStyleBackColor = true;
+            this.satu.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // enam
+            // 
+            this.enam.Location = new System.Drawing.Point(113, 137);
+            this.enam.Name = "enam";
+            this.enam.Size = new System.Drawing.Size(30, 23);
+            this.enam.TabIndex = 9;
+            this.enam.Text = " 6";
+            this.enam.UseVisualStyleBackColor = true;
+            this.enam.Click += new System.EventHandler(this.enam_Click);
+            // 
+            // lima
+            // 
+            this.lima.Location = new System.Drawing.Point(77, 137);
+            this.lima.Name = "lima";
+            this.lima.Size = new System.Drawing.Size(30, 23);
+            this.lima.TabIndex = 8;
+            this.lima.Text = " 5";
+            this.lima.UseVisualStyleBackColor = true;
+            this.lima.Click += new System.EventHandler(this.lima_Click);
+            // 
+            // empat
+            // 
+            this.empat.Location = new System.Drawing.Point(41, 137);
+            this.empat.Name = "empat";
+            this.empat.Size = new System.Drawing.Size(30, 23);
+            this.empat.TabIndex = 7;
+            this.empat.Text = "4";
+            this.empat.UseVisualStyleBackColor = true;
+            this.empat.Click += new System.EventHandler(this.empat_Click);
+            // 
+            // sembilan
+            // 
+            this.sembilan.Location = new System.Drawing.Point(113, 166);
+            this.sembilan.Name = "sembilan";
+            this.sembilan.Size = new System.Drawing.Size(30, 23);
+            this.sembilan.TabIndex = 12;
+            this.sembilan.Text = " 9";
+            this.sembilan.UseVisualStyleBackColor = true;
+            this.sembilan.Click += new System.EventHandler(this.sembilan_Click);
+            // 
+            // delapan
+            // 
+            this.delapan.Location = new System.Drawing.Point(77, 166);
+            this.delapan.Name = "delapan";
+            this.delapan.Size = new System.Drawing.Size(30, 23);
+            this.delapan.TabIndex = 11;
+            this.delapan.Text = " 8";
+            this.delapan.UseVisualStyleBackColor = true;
+            this.delapan.Click += new System.EventHandler(this.delapan_Click);
+            // 
+            // tujuh
+            // 
+            this.tujuh.Location = new System.Drawing.Point(41, 166);
+            this.tujuh.Name = "tujuh";
+            this.tujuh.Size = new System.Drawing.Size(30, 23);
+            this.tujuh.TabIndex = 10;
+            this.tujuh.Text = "7";
+            this.tujuh.UseVisualStyleBackColor = true;
+            this.tujuh.Click += new System.EventHandler(this.tujuh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(177, 239);
+            this.Controls.Add(this.sembilan);
+            this.Controls.Add(this.delapan);
+            this.Controls.Add(this.tujuh);
+            this.Controls.Add(this.enam);
+            this.Controls.Add(this.lima);
+            this.Controls.Add(this.empat);
+            this.Controls.Add(this.tiga);
+            this.Controls.Add(this.dua);
+            this.Controls.Add(this.satu);
+            this.Controls.Add(this.nol);
+            this.Controls.Add(this.sama_dengan);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.hasil);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label hasil;
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.Button sama_dengan;
+        private System.Windows.Forms.Button nol;
+        private System.Windows.Forms.Button tiga;
+        private System.Windows.Forms.Button dua;
+        private System.Windows.Forms.Button satu;
+        private System.Windows.Forms.Button enam;
+        private System.Windows.Forms.Button lima;
+        private System.Windows.Forms.Button empat;
+        private System.Windows.Forms.Button sembilan;
+        private System.Windows.Forms.Button delapan;
+        private System.Windows.Forms.Button tujuh;
     }
 }
 
--- modul2_1302213020/Form1.cs
+++ modul2_1302213020/Form1.cs
@@ -16,5 +16,96 @@ namespace modul2_1302213020
         {
             InitializeComponent();
         }
+
+        private int hasilKalkulator = 0;
+        private string kalkulatorLabel = "";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 0;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 1;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            if (this.kalkulatorLabel != "") {
+                this.hasilKalkulator += int.Parse(hasil.Text);
+            }
+            this.kalkulatorLabel = "";
+            hasil.Text = "0";
+        }
+
+        private void sama_dengan_Click(object sender, EventArgs e)
+        {
+            if (this.kalkulatorLabel != "")
+            {
+                this.hasilKalkulator += int.Parse(this.kalkulatorLabel);
+            }
+            this.kalkulatorLabel = "";
+            hasil.Text = this.hasilKalkulator.ToString();
+        }
+
+        private void dua_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 2;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void tiga_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 3;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void empat_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 4;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void lima_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 5;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void enam_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 6;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void tujuh_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 7;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void delapan_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 8;
+            hasil.Text = kalkulatorLabel;
+        }
+
+        private void sembilan_Click(object sender, EventArgs e)
+        {
+            this.kalkulatorLabel += 9;
+            hasil.Text = kalkulatorLabel;
+        }
     }
 }
